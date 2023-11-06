0adc159f8225a8d744eeaafa03c710357743ef68
vinodliyanage <vinodsliyanage@gmail.com>
2023-11-06 09:32:28 +0530

    add images.

---
+++ src/constants/images.js
@@ -0,0 +1,15 @@
+import qrcode from "../assets/qrcode.png";
+import close from "../assets/close.svg";
+import confetti from "../assets/confetti.png";
+import loading from "../assets/loading.gif";
+import error from "../assets/error.gif";
+
+const images = {
+  qrcode,
+  close,
+  confetti,
+  loading,
+  error,
+};
+
+export default images;
