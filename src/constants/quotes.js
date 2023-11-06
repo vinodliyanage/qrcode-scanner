ac56481e82dbfe54bb5008d12a00d245e2fcefd3
vinodliyanage <vinodsliyanage@gmail.com>
2023-11-06 09:33:12 +0530

    add quotes.

---
+++ src/constants/quotes.js
@@ -0,0 +1,17 @@
+const quotes = [
+  "Best of luck <teamname> - may your lines of code create wonders!",
+  "In the race for innovation, <teamname> is geared up to lead the coding sprint. May your keystrokes be swift and your logic be flawless!",
+  "<teamname>, may your code be as elegant as a well-crafted algorithm and as powerful as optimized runtime. Good luck at CodeRush!",
+  "To the intrepid coders of <teamname>, may your code flow seamlessly, solving puzzles and racing ahead at CodeRush.",
+  "Harness the power of logic, wield the strength of syntax - go, <teamname>, and conquer CodeRush with your coding prowess!",
+  "CodeRush awaits, <teamname>! Let your algorithms sparkle, your functions shine, and your debugging be swift!",
+  "May the lines of code written by <teamname> at CodeRush be an ode to creativity, speed, and ingenious solutions.",
+  "In the fast lane of coding challenges, <teamname> drives towards victory at CodeRush. Best of luck, programmers!",
+  "The coding adrenaline is high, and <teamname> is poised for an epic run at CodeRush. May your debugging be minimal and success maximal!",
+  "<teamname>, prepare to rev up your coding engines and set the pace for brilliance at CodeRush - may your logic be sound and your functions optimized!",
+  "As you navigate the twists and turns of CodeRush, <teamname>, may your algorithms be agile and your solutions innovative.",
+];
+
+
+
+export default quotes;
